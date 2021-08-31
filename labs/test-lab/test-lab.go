405883b8502760bc2d9ecd4cc0d02138d55febea
Luis99B <luisabv@hotmail.es>
2021-08-31 22:19:39 +0000

    Solve test-lab

--- labs/test-lab/test-lab.go
+++ labs/test-lab/test-lab.go
@@ -2,8 +2,18 @@ package main
 
 import (
 	"fmt"
+	"os"
 )
 
 func main() {
-	fmt.Println("Welcome to the jungle")
+	n := len(os.Args[1:])
+	if n < 1 {
+		fmt.Printf("ERROR, no name given\n")
+	} else {
+		fmt.Printf("Hello")
+		for i := 0; i < n; i++ {
+			fmt.Printf(" %s", os.Args[i+1])
+		}
+		fmt.Printf("\n")
+	}
 }

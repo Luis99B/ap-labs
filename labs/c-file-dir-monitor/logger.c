26da3cc7e66b6e85032f670eb25916f7d2777dc0
Luis99B <luisabv@hotmail.es>
2021-11-11 05:06:34 +0000

    Solve file_dir_monitor

--- labs/c-file-dir-monitor/logger.c
+++ labs/c-file-dir-monitor/logger.c
@@ -1,23 +1,56 @@
 #include <stdio.h>
 #include "logger.h"
+#include <stdarg.h>
+
+// ANSI color codes
+#define NORMAL "\e[0m" // default
+#define LOG "\e[0;34m" // blue
+#define INFO "\e[0;32m" // green
+#define WARNING "\e[0;33m" // yellow
+#define ERROR "\e[0;31m" // red
+#define PANIC "\e[0;35m" // purple
 
 int initLogger(char *logType) {
-    printf("Initializing Logger on: %s\n", logType);
+    printf("%sInitializing Logger on:%s %s\n", LOG, NORMAL, logType);
     return 0;
 }
 
 int infof(const char *format, ...) {
+    va_list ap;
+    va_start(ap, format);
+    printf("%sINFO:%s ", INFO, NORMAL);
+    vprintf(format, ap);
+    printf("\n");
+    va_end(ap);
     return 0;
 }
 
 int warnf(const char *format, ...) {
+    va_list ap;
+    va_start(ap, format);
+    printf("%sWARNING:%s ", WARNING, NORMAL);
+    vprintf(format, ap);
+    printf("\n");
+    va_end(ap);
     return 0;
 }
 
 int errorf(const char *format, ...) {
-    return 0;
-}
-int panicf(const char *format, ...) {
+    va_list ap;
+    va_start(ap, format);
+    printf("%sERROR:%s ", ERROR, NORMAL);
+    vprintf(format, ap);
+    printf("\n");
+    va_end(ap);
     return 0;
 }
 
+int panicf(const char *format, ...) {
+    va_list ap;
+    va_start(ap, format);
+    printf("%sPANIC:%s ", PANIC, NORMAL);
+    vprintf(format, ap);
+    printf("\n");
+    va_end(ap);
+    return 0;
+}
--- labs/c-file-dir-monitor/monitor.c
+++ labs/c-file-dir-monitor/monitor.c
@@ -1,7 +1,99 @@
+#define _XOPEN_SOURCE 500
+
 #include <stdio.h>
 #include "logger.h"
+#include <ftw.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdint.h>
+#include <limits.h>
+#include <sys/inotify.h>
 
-int main(){
-    // Place your magic here
+#define BUFFER_LEN 1024 * (sizeof(struct inotify_event) + NAME_MAX + 1)
+
+int inotify_fd;
+
+static int display_info(const char *fpath, const struct stat *sb, int tflag, struct FTW *ftwbuf) {
+    int n = 0;
+    if(tflag == FTW_D){
+        n = inotify_add_watch(inotify_fd, fpath, IN_MOVE | IN_CREATE | IN_DELETE);
+        if(n == -1){
+            errorf("Display info inotify error");
+        }
+    }
+    return n;
+}
+
+int main(int argc, char** argv){
+    if (argc < 2){
+        warnf("Not enough arguments");
+        exit(-1);
+    }
+    char *path = argv[1];
+    inotify_fd = inotify_init();
+    if (inotify_fd == -1){
+        errorf("Could not initiate inotify");
+        exit(-1);
+    }
+    int flags = 0;
+    if (strchr(path, 'd') != NULL){
+        flags |= FTW_DEPTH;
+    }
+    if (strchr(path, 'p') != NULL){
+        flags |= FTW_PHYS;
+    }
+    int inotify_watch = nftw(path, display_info, 20, flags);
+    if (inotify_watch == -1){
+        errorf("Could not add a watch to inotify");
+        exit(-1);
+    }
+    initLogger("stdout");
+    infof("Starting File/Directory Monitor on %s", path);
+    char buffer[BUFFER_LEN];
+    ssize_t num_r;
+    for (;;){
+        num_r = read(inotify_fd, buffer, BUFFER_LEN);
+        if (num_r == -1){
+            panicf("There has been an error in read");
+            exit(-1);
+        } else if (num_r == 0) {
+            panicf("There is nothing to read in inotify");
+            exit(-1);
+        }
+        char *p;
+        for (p = buffer; p < buffer + num_r;){
+            struct inotify_event *event = (struct inotify_event *) p;
+            if (event -> len){
+                if (event -> mask & IN_CREATE){
+                    if (event -> mask & IN_ISDIR){
+                        infof("- [Directory - Create] - %s", event -> name);
+                    } else {
+                        infof("- [File - Create] - %s", event -> name);
+                    }
+                } else if (event -> mask & IN_DELETE){
+                    if (event -> mask & IN_ISDIR){
+                        infof("- [Directory - Removal] - %s", event -> name);
+                    } else {
+                        infof("- [File - Removal] - %s", event -> name);
+                    }
+                } else if (event -> cookie != 0){
+                    if (event -> mask & IN_MOVED_FROM){
+                        if(event -> mask & IN_ISDIR){
+                            infof("- [Directory - Rename] - %s -> ", event -> name);
+                        } else {
+                            infof("- [File - Rename] - %s -> ", event -> name);
+                        }
+                    } else {
+                        infof("%s",event -> name);
+                    }
+                } else {
+                    errorf("error");
+                }
+                p +=  sizeof(struct inotify_event) + event -> len;
+            }
+        }
+    }
+    inotify_rm_watch(inotify_fd, inotify_watch);
+    close(inotify_fd);
     return 0;
 }
